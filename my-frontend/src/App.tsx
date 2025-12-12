06316fd3b17e310c82f69909ed165f1f04aeae89
ynhule96 <Lenhuy10011996@gmail.com>
2025-12-12 14:52:26 -0700

    setup frontend and backkend

---
+++ my-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './index.css'
+function App() {
+
+
+  return (
+   <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-red-600">Hello React + Tailwind!</h1>
+    </div>
+  )
+}
+
+export default App
